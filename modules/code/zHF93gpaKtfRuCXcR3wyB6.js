ee6b267836097dbe8cc6525d251e7e9f5698c434
dotJS <justdotJS@icloud.com>
2017-10-23 09:03:07 +0800

    Create zHF93gpaKtfRuCXcR3wyB6.js

---
+++ modules/code/zHF93gpaKtfRuCXcR3wyB6.js
@@ -0,0 +1,30 @@
+const Command = require('../../cmdModule/commands').Command
+
+class codeCommand extends Command {
+  constructor() {
+    super({
+      name: 'zHF93gpaKtfRuCXcR3wyB6',
+      help: '--',
+      lhelp: '--'
+    })
+  }
+
+  hasPermission(message) {
+    if (message.guild.id == '268970339948691456') return true
+    return false
+  }
+
+  async run(msg, args, api) {
+    var user = msg.guild.member(msg.author);
+    var role = msg.guild.roles.find('name', '#code Stage 2')
+
+    user.addRole(role)
+    
+    msg.delete()
+
+    api.embed('#00ff00', `<:Tick:318378431051989003> \`${user.user.username} has moved into #code Stage 2!\``, '')
+  }
+
+}
+
+module.exports = codeCommand
