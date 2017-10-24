f22754533731d5f2f5f359c33a5c8dee9d5781a1
dotJS <justdotJS@icloud.com>
2017-10-24 14:08:31 +0800

    Update search.js

--- modules/fun/search.js
+++ modules/fun/search.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
-var imageSearch = require('node-google-image-search');
+const GoogleImages = require('google-images');
+const client = new GoogleImages('014338422944177884073:dkjfxeuq_dy', 'AIzaSyB2AcbLRMT-gTe6T75wYBx4JCarYBe7pBE');
 
 const Command = require('../../cmdModule/commands').Command
 
@@ -12,22 +13,39 @@ class srCommand extends Command {
   }
 
   async run(message, args, api) {
+	
+
+args.splice(0,1);
+var argsg = args.join(' ');
+
+	  
+client.search(argsg)
+	.then(images => {
+		/*
+		[{
+			"url": "http://steveangello.com/boss.jpg",
+			"type": "image/jpeg",
+			"width": 1024,
+			"height": 768,
+			"size": 102451,
+			"thumbnail": {
+				"url": "http://steveangello.com/thumbnail.jpg",
+				"width": 512,
+				"height": 512
+			}
+		}]
+		 */
 	let embed = new Discord.RichEmbed()
       embed.setTitle('<:apple_face_sunglasses:359559678809866240> `Image Found Successfully`')
 	  embed.setDescription(String.fromCharCode(8203))
       embed.setColor('#00ff00')
       //embed.setTimestamp()
 	  embed.setFooter('Replying to ' + message.author.tag)
-
-args.splice(0,1);
-var argsg = args.join(' ');
-var offset = Math.floor((Math.random() * 100) + 1);
-var results = imageSearch(argsg, callback, offset, 1);
- 
-function callback(results) {
-    embed.setImage(results[0]);
+	embed.setImage(images[0].url);
 	message.channel.send({ embed })
-}
+	});
+ 
+    
 
 //	embed.setImage(img)
 
