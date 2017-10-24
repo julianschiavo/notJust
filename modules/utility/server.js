e0f23c894dd53065bcedb39259cec327b9a7cf53
dotJS <justdotJS@icloud.com>
2017-10-24 19:07:04 +0800

    Update server.js

--- modules/utility/server.js
+++ modules/utility/server.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js')
 const Command = require('../../cmdModule/commands').Command
+const ta = require('timeago')
 
 class serverCommand extends Command {
   constructor() {
@@ -20,17 +21,20 @@ class serverCommand extends Command {
     var emojis   = message.guild.emojis.size
     var name     = message.guild.name
     var owner    = message.guild.owner.user.tag
-    var gid    = message.guild.id
+    var gid      = message.guild.id
+    var time     = ta.ago(message.guild.createdTimestamp);
     //let online   = this.client.users.filter(u => u.status != "offline").length;
     let embed = new Discord.RichEmbed()
       embed.setTitle('<:apple_symbol_info:359559750096257024> `About ' + name + '`')
       embed.setColor('#00ff00')
     embed.setFooter('Replying to ' + message.author.tag)
-        embed.addField('`ID`', gid, true)
-        embed.addBlankField(true)
+        /*embed.addField('`ID`', gid, true)
+        embed.addBlankField(true)*/
         embed.setDescription(String.fromCharCode(8203))
+        
         embed.addField('`Emoji`', emojis, true)
         embed.addField('`Owner`', owner, true)
+        embed.addField('`Created`', time, true)
         embed.addBlankField(true)
         embed.addField('`Channels`', channels, true)
         embed.addField('`Members`', users, true)
