50576d238f094a84a214bc7469e990566eeec5e3
dotJS <justdotJS@icloud.com>
2017-10-25 10:14:22 +0800

    Update search.js

--- modules/fun/search.js
+++ modules/fun/search.js
@@ -19,8 +19,9 @@ class srCommand extends Command {
 	async run(message, args, api) {
 		args.splice(0, 1);
 		var argsg = args.join(' ');
+
 		function randomN(min, max) {
-					return Math.floor(Math.random() * (max - min + 1) + min);
+			return Math.floor(Math.random() * (max - min + 1) + min);
 		}
 		if (argsg.indexOf('img') >= 0 || argsg.indexOf('[img]') >= 0) {
 			var query = argsg.replace('[img]', '').replace('img', '')
@@ -59,23 +60,23 @@ class srCommand extends Command {
 
 			bing.list({
 					keyword: query,
-					num: 10,
+					num: 100,
 					detail: true
 				})
 				.then(function(res) {
 					//console.log('first 10 results from bing', res[1].url);
-				let embed = new Discord.RichEmbed()
-				embed.setTitle('<:apple_face_sunglasses:359559678809866240> `Image Found Successfully`')
-				embed.setDescription(String.fromCharCode(8203))
-				embed.setColor('#00ff00')
-				//embed.setTimestamp()
-				embed.setFooter('Replying to ' + message.author.tag)
-				//message.channel.send(message)
-				var rN = randomN(1,100)
-				embed.setImage(res[rN].url);
-				message.channel.send({
-					embed
-				})
+					let embed = new Discord.RichEmbed()
+					embed.setTitle('<:apple_face_sunglasses:359559678809866240> `Image Found Successfully`')
+					embed.setDescription(String.fromCharCode(8203))
+					embed.setColor('#00ff00')
+					//embed.setTimestamp()
+					embed.setFooter('Replying to ' + message.author.tag)
+					//message.channel.send(message)
+					var rN = randomN(1, 100)
+					embed.setImage(res[rN].url);
+					message.channel.send({
+						embed
+					})
 				}).catch(function(err) {
 					console.log('Error: ', err);
 				})
