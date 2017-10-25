5b511efc094b61726df4129008f23ab83e330dc6
dotJS <justdotJS@icloud.com>
2017-10-25 09:29:44 +0800

    Update search.js

--- modules/fun/search.js
+++ modules/fun/search.js
@@ -1,56 +1,72 @@
-const Discord = require('discord.js');
-var PythonShell = require('python-shell');
+const Discord = require('discord.js'),
+	cheerio = require('cheerio'),
+	snekfetch = require('snekfetch'),
+	querystring = require('querystring');
 
 const Command = require('../../cmdModule/commands').Command
 
 class srCommand extends Command {
-  constructor() {
-    super({
-      name: 'search',
-      help: 'Search for an image'
-    })
-  }
+	constructor() {
+		super({
+			name: 'search',
+			help: 'Search text or an image',
+			lhelp: '[img] {query}\n[img] is whether to search for an image or just text, it can be included anywhere in the command.\n{query} is the query to search for.'
+		})
+	}
 
-  async run(message, args, api) {
-	
+	async run(message, args, api) {
+		args.splice(0, 1);
+		var argsg = args.join(' ');
+		var image
+		if (argsg.indexOf('img') >= 0 || argsg.indexOf('[img]') >= 0) {
+			var query = argsg.replace('[img]', '').replace('img', '')
+			//https://www.google.com.hk/search?q=hi&tbm=isch
+			let searchUrl = `https://www.google.com/search?q=${encodeURIComponent(argsg)}&tbm=isch`;
 
-args.splice(0,1);
-var argsg = args.join(' ');
+			// We will now use snekfetch to crawl Google.com. Snekfetch uses promises so we will
+			// utilize that for our try/catch block.
+			return snekfetch.get(searchUrl).then((result) => {
 
-	  
-/*var spawn = require('child_process').spawn,
-    py    = spawn('python', ['search.py']);*/
+				// Cheerio lets us parse the HTML on our google result to grab the URL.
+				let $ = cheerio.load(result.text);
 
-var options = {
-scriptPath: '/home/me/notJust/modules/fun/',
-args: argsg
-};
-var pyshell = new PythonShell('search.py', options);
+				// This is allowing us to grab the URL from within the instance of the page (HTML)
+				//let googleData = $('.r').first().find('a').first().attr('href');
+				function randomN(min, max) {
+					return Math.floor(Math.random() * (max - min + 1) + min);
+				}
+				var googleData = $('.r').first()
+				var rNum = randomN(1, 100)
+				googleData = googleData.find('img')
+				googleData = googleData[rNum].attr('src');
 
-pyshell.on('message', function (message) {	  
-  let embed = new Discord.RichEmbed()
-      embed.setTitle('<:apple_face_sunglasses:359559678809866240> `Image Found Successfully`')
-	  embed.setDescription(String.fromCharCode(8203))
-      embed.setColor('#00ff00')
-      //embed.setTimestamp()
-	  embed.setFooter('Replying to ' + message.author.tag)
-	  message.channel.send(message)
-	embed.setImage(message);
-	message.channel.send({ embed })
-});
-//py.stdin.end();
-  
-	  
-	
- 
-    
+				// Now that we have our data from Google, we can send it to the channel.
+				googleData = querystring.parse(googleData.replace('/url?', ''));
+				//searchMessage.edit(`Result found!\n${googleData.q}`);
+				let embed = new Discord.RichEmbed()
+				embed.setTitle('<:apple_face_sunglasses:359559678809866240> `Image Found Successfully`')
+				embed.setDescription(String.fromCharCode(8203))
+				embed.setColor('#00ff00')
+				//embed.setTimestamp()
+				embed.setFooter('Replying to ' + message.author.tag)
+				message.channel.send(message)
+				embed.setImage(message);
+				message.channel.send({
+					embed
+				})
+				// If no results are found, we catch it and return 'No results are found!'
+			}).catch((err) => {
+				api.error('No results found')
+			});
 
-//	embed.setImage(img)
 
 
-      
-    return true
-  }
+		}
+
+
+
+		return true
+	}
 }
 
 module.exports = srCommand
