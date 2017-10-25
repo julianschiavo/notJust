7e67584546205db1f03959056af8318e73046064
dotJS <justdotJS@icloud.com>
2017-10-25 10:27:58 +0800

    Update search.js

--- modules/fun/search.js
+++ modules/fun/search.js
@@ -26,9 +26,6 @@ class srCommand extends Command {
 		}
 		if (argsg.indexOf('img') >= 0 || argsg.indexOf('[img]') >= 0) {
 			var query = argsg.replace('[img]', '').replace('img', '')
-			if (!query) {
-				api.error('Please specify a query.')
-			}
 			//query = encodeURIComponent(query)
 			//https://www.google.com.hk/search?q=hi&tbm=isch
 			//let searchUrl = 'https://www.google.com/search?q=' + query + '&tbm=isch';
@@ -82,13 +79,13 @@ class srCommand extends Command {
 						embed
 					})
 				}).catch(function(err) {
-				/*if (err.indexOf('undefined') >= 0) {
+				if (err.indexOf('undefined') >= 0) {
 					api.error('No results were returned for that query, or it was an invalid query.')
 				} else if err.indexOf('no keyword') >= 0) {
 					api.error('Please specify a query.')
 				} else {
 					api.error(err);
-				}*/
+				}
 				})
 
 
