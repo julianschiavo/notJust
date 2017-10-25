41c46a6d7073e186e3a6274349564aac7253666d
dotJS <justdotJS@icloud.com>
2017-10-25 09:48:01 +0800

    Update search.js

--- modules/fun/search.js
+++ modules/fun/search.js
@@ -36,7 +36,7 @@ class srCommand extends Command {
 				function randomN(min, max) {
 					return Math.floor(Math.random() * (max - min + 1) + min);
 				}
-				console.log($('.y').first())
+				message.channel.send($)
 				var googleData = $('.y').first().find('ivg-i').first().find('a').first().attr('src');
 
 				var rNum = randomN(1, 100)
