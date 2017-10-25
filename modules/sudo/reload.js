11bb1c9e822a7ec5e96ccd9976b677afd6d1ea0d
dotJS <justdotJS@icloud.com>
2017-10-25 15:17:19 +0800

    Update reload.js

--- modules/sudo/reload.js
+++ modules/sudo/reload.js
@@ -1,60 +1,40 @@
-
-  
-  const Discord = require('discord.js');
+const Discord = require('discord.js');
 
 const Command = require('../../cmdModule/commands').Command
 
 class reloadCommand extends Command {
-  constructor() {
-    super({
-      name: 'reload',
-      help: 'Reload a command file',
-      lhelp: '{{module_folder_name}/{command_name}}\n{module_folder_name} is the name of the folder for the module for the command to be reloaded\n{command_name} is the command to reload 
-    })
-  }
-  
-  hasPermission(message) {
-    if (message.author.id == require('../../config.json').owner) return true
-    return false
-  }
+	constructor() {
+		super({
+			name: 'reload',
+			help: 'Reload a command file',
+			lhelp: '{module_folder} {command}\n{module_folder} is the name of the folder for the module for the command to be reloaded\n{command} is the command to reload'
+		})
+	}
 
-  async run(message, args, api) {
-	let embed = new Discord.RichEmbed()
-      embed.setTitle('<:apple_animal_cat:372237719780196353> `Cat Fetched`')
-	  embed.setDescription(String.fromCharCode(8203))
-      embed.setColor('#00ff00')
-      //embed.setTimestamp()
-	  embed.setFooter('Replying to ' + message.author.tag)
-      var img
-      var img2
-    request({
-        url: url,
-        json: true
-    }, function (error, response, body) {
-        console.log(body.file);
-	//var img = body.file;
-	    //var bod = body
-	    var check = checkURL(body.file)
-    if (check) {
-	    } else {
-	    api.error('Wrong file format returned. Please try again.')
-		    return;
-    }
-	    var img = body.file
-	    var img2 = encodeURI(body.file);
-	embed.setImage(img)
-    })
-	  
-	  /*get(url).then(response => {
-	embed.setImage(response.body.file)
-	}*/
+	hasPermission(message) {
+		if (message.author.id == require('../../config.json').owner) return true
+		return false
+	}
 
-      //message.channel.send(bod)
-        message.channel.send(img)
-	  message.channel.send(img2)
-      message.channel.send({ embed })
-    return true
-  }
+	async run(message, args, api) {
+		if (!args[1] || !args[2]) {
+			api.error('Please specify a module folder and command')
+		}
+		var mn = args[1]
+		var cn = args[2]
+		delete require.cache[require.resolve('../' + mn + '/' + cn + '.js')];
+                //message.reply(`The command ${args[0]} has been reloaded`);
+		let embed = new Discord.RichEmbed()
+		embed.setTitle('<:green_tick:330712173288488960> `' + cn + ' Reloaded`') 
+		embed.setDescription(String.fromCharCode(8203)) 
+		embed.setColor('#00ff00')
+		//embed.setTimestamp()
+		embed.setFooter('Replying to ' + message.author.tag) 
+
+		message.channel.send({
+			embed
+		}) return true
+	}
 }
 
 module.exports = catCommand
