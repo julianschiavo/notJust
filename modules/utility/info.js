0053d5d53ce5028b72ba77e2f5fccd7f3fbed392
dotJS <justdotJS@icloud.com>
2017-10-26 16:46:15 +0800

    Update info.js

--- modules/utility/info.js
+++ modules/utility/info.js
@@ -7,11 +7,11 @@ class infoCommand extends Command {
 		super({
 			name: 'info',
 			help: 'Get user/server/role/channel info',
-			lhelp: '{server/user/role} [user/role name]\n{server/user/role} is whether to get info about the server, a user, or a role\n[user/role name] is the user or to get info on (if user, defaults to message author)'
+			lhelp: '{server/user/role/channel} [channel/user/role name]\n{server/user/role/channel} is what to get info about\n[channel/user/role name] is the name or ID of what you want info about'
 		})
 	}
 	hasPermission(message) {
-		if (!message.guild && message.channel.type !== 'text') return false
+		if (!message.guild || message.channel.type !== 'text') return false
 		return true
 	}
 
@@ -19,7 +19,7 @@ class infoCommand extends Command {
 		args.splice(0, 1);
 		var argg
 		if (!args[0]) {
-			api.error('Please specify if you want info on the server or a user!')
+			api.error('Please specify if you want info on the server, a user, or a role.')
 		} else {
 			argg = args[0].toLowerCase()
 		}
@@ -78,6 +78,46 @@ class infoCommand extends Command {
 			message.channel.send({
 				embed
 			})
+		} else if (argg.indexOf('channel') >= 0) {
+			var channel
+
+			function isN(num) {
+				return !isNaN(num)
+			}
+			args.splice(0, 1)
+			var argsF = args.join(' ')
+			if (argsF) {
+				if (!isN(argsF)) {
+					channel = message.guild.channels.find("name", argsF)
+				} else {
+					channel = message.guild.channels.get(argsF)
+				}
+			} else {
+				api.error('Please specify a channel ID or name.')
+			}
+			var pos = channel.position
+			var icon = message.guild.iconURL
+			var time = ta.ago(channel.createdTimestamp);
+			var id = channel.id
+			var type = channel.type
+			if (type == 'text') {
+				type = 'Text Channel'
+			} else if (type == 'voice') {
+				type = 'Voice Channel'
+			}
+			var name = channel.name
+			embed.setTitle('<:apple_symbol_info:359559750096257024> `About ' + name + '`')
+			embed.setColor('#00ff00')
+			embed.setFooter('Replying to ' + message.author.tag)
+			embed.setDescription(String.fromCharCode(8203))
+			embed.setThumbnail(icon)
+			embed.addField('`Type`', type, false)
+			embed.addField('`Position`', pos, true)
+			embed.addField('`Created`', time, true)
+			embed.addField('`ID`', id, false)
+			message.channel.send({
+				embed
+			})
 		} else if (argg.indexOf('role') >= 0) {
 			var role
 			args.splice(0, 1)
@@ -111,17 +151,8 @@ class infoCommand extends Command {
 			embed.addField('`ID`', id, false)
 			embed.addField('`Position`', pos, true)
 			embed.addField('`Hoisted`', hoist, true)
-
 			embed.addField('`Color`', color, true)
 			embed.addField('`Created`', time, true)
-
-
-
-
-
-			//embed.addField('`Members`', users, false)
-
-			//embed.addField('`Joined`', join, true)
 			message.channel.send({
 				embed
 			})
