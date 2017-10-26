ffbca6cf18eff900db822bb87493d90ed07a96a4
dotJS <justdotJS@icloud.com>
2017-10-26 17:36:31 +0800

    Update about.js

--- modules/utility/about.js
+++ modules/utility/about.js
@@ -25,9 +25,10 @@ class aboutCommand extends Command {
 		embed.setFooter('Replying to ' + message.author.tag)
 		embed.setDescription(String.fromCharCode(8203))
 		embed.addField('`Stats`', 'notJust is on ' + servers + ' servers, which contain ' + channels + ' channels, and ' + users + ' users.', false)
-		embed.addField('`Invite`', 'notJust is currently private as it is not finished and may be buggy.', false)
-		embed.addField('`Description`', 'notJust is a state-of-the-art bot made by dotJS. It has modules which include fun commands, moderation commands, and more.', false)
-		embed.addField('`Contributors`', 'notJust couldn\'t have been built without the assistance of Vlad, so thanks for all his help.', false)
+		embed.addField('`Invite`', '[Invite notJust by clicking here.](https://discordapp.com/oauth2/authorize?client_id=329772339967426560&scope=bot&permissions=8) \nWe HIGHLY recommend you give it the "Administrator" permission to ensure all commands work.', false)
+		embed.addField('`Premium`', 'I, the founder of notJust, am just 13 and have to run my servers and sites off donations. \nGet **notJust Premium** - and support me - by donating. [Just press here!](https://patreon.com/justdotJS)', false)
+		embed.addField('`Description`', 'notJust is a leading state-of-the-art Discord bot founded by dotJS. \nIt has modules which include fun commands, music commands, moderation commands, and more.', false)
+		embed.addField('`Contributors`', 'notJust couldn\'t have been built without the assistance of `King - Vlad` and `szymex73`, so thanks for all their help.', false)
 		message.channel.send({
 			embed
 		})
