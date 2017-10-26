78d2c3ce6c1b2ddd3504963061a6496ccad90580
dotJS <justdotJS@icloud.com>
2017-10-26 08:12:13 +0800

    Update kick.js

--- modules/admin/kick.js
+++ modules/admin/kick.js
@@ -23,7 +23,9 @@ class kickCommand extends Command {
 		}
     var arg = args[0]
     var user = api.getUser(arg,'member')
-
+if (user.user.id == message.author.id) {
+	api.error('You can\'t ban yourself!')
+		  }
     args.splice(0,1)
 		var reason
     if (args[0]) {
