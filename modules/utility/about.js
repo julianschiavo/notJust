bc683d7967a1a3c6bb90000ef66cf5ddb0db5f7b
dotJS <justdotJS@icloud.com>
2017-10-26 16:13:44 +0800

    Update about.js

--- modules/utility/about.js
+++ modules/utility/about.js
@@ -2,35 +2,37 @@ const Discord = require('discord.js')
 const Command = require('../../cmdModule/commands').Command
 
 class aboutCommand extends Command {
-  constructor() {
-    super({
-      name: 'about',
-      help: 'Get information about the bot'
-    })
-  }
+	constructor() {
+		super({
+			name: 'about',
+			help: 'Get bot info'
+		})
+	}
 
-  async run(message, args, api) {
-    //message.delete()
-    //api.evalembed([245, 236, 71], "🏓 `Ping Successful`", "Heartbeat", one, "Ping", two)
-    var servers  = message.client.guilds.size
-    var channels = message.client.channels.size
-    var users    = message.client.users.size
-    //let online   = this.client.users.filter(u => u.status != "offline").length;
-    let embed = new Discord.RichEmbed()
-      //embed.setTimestamp()
-      embed.setTitle('<:apple_symbol_info:359559750096257024> `About notJust`')
-      embed.setColor('#00ff00')
-      //embed.setFooter('Replying to: ' + this.message.author.tag)
-      //embed.setTimestamp()
-    embed.setFooter('Replying to ' + message.author.tag)
-        embed.setDescription(String.fromCharCode(8203))
-        embed.addField('`Stats`', 'notJust is on ' + servers + ' servers, which contain ' + channels + ' channels, and ' + users + ' users.', false)
-        embed.addField('`Invite`', 'notJust is currently private as it is not finished and may be buggy.', false)
-        embed.addField('`Description`', 'notJust is a state-of-the-art bot made by dotJS. It has modules which include fun commands, moderation commands, and more.', false)
-        embed.addField('`Contributors`', 'notJust couldn\'t have been built without the assistance of Vlad, so thanks for all his help.', false)
-      message.channel.send({ embed })
-    return true
-  }
+	async run(message, args, api) {
+		//message.delete()
+		//api.evalembed([245, 236, 71], "🏓 `Ping Successful`", "Heartbeat", one, "Ping", two)
+		var servers = message.client.guilds.size
+		var channels = message.client.channels.size
+		var users = message.client.users.size
+		//let online   = this.client.users.filter(u => u.status != "offline").length;
+		let embed = new Discord.RichEmbed()
+		//embed.setTimestamp()
+		embed.setTitle('<:apple_symbol_info:359559750096257024> `About notJust`')
+		embed.setColor('#00ff00')
+		//embed.setFooter('Replying to: ' + this.message.author.tag)
+		//embed.setTimestamp()
+		embed.setFooter('Replying to ' + message.author.tag)
+		embed.setDescription(String.fromCharCode(8203))
+		embed.addField('`Stats`', 'notJust is on ' + servers + ' servers, which contain ' + channels + ' channels, and ' + users + ' users.', false)
+		embed.addField('`Invite`', 'notJust is currently private as it is not finished and may be buggy.', false)
+		embed.addField('`Description`', 'notJust is a state-of-the-art bot made by dotJS. It has modules which include fun commands, moderation commands, and more.', false)
+		embed.addField('`Contributors`', 'notJust couldn\'t have been built without the assistance of Vlad, so thanks for all his help.', false)
+		message.channel.send({
+			embed
+		})
+		return true
+	}
 }
 
 module.exports = aboutCommand
