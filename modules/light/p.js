c33a2efa12a24e8980a89ed6b973ce31b5c6031f
dotJS <justdotJS@icloud.com>
2017-10-26 16:29:34 +0800

    Update p.js

--- modules/light/p.js
+++ modules/light/p.js
@@ -10,7 +10,7 @@ class LpresCommand extends Command {
 	}
 
 	hasPermission(message) {
-		if (message.guild.id == '277006003797491712') return true
+		if (message.guild && message.guild.id == '277006003797491712') return true
 		return false
 	}
 
