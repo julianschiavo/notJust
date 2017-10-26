8c85fc028abe0b8b710c28feffba7a1fec69df23
dotJS <justdotJS@icloud.com>
2017-10-26 16:00:24 +0800

    Update kick.js

--- modules/admin/kick.js
+++ modules/admin/kick.js
@@ -12,33 +12,33 @@ class kickCommand extends Command {
 
 	hasPermission(message) {
 		//if (message.author.id == require('../../config.json').owner) return true
-    if (message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return true
+		if (message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return true
 		return false
 	}
 
 	async run(message, args, api) {
-    args.splice(0,1)
+		args.splice(0, 1)
 		if (!args[0]) {
 			api.error('Please specify which user to kick.')
 		}
-    var arg = args[0]
-    var user = api.getUser(arg,'member')
-if (user.user.id == message.author.id) {
-	api.error('You can\'t kick yourself!')
-	return
-		  }
+		var arg = args[0]
+		var user = api.getUser(arg, 'member')
+		if (user.user.id == message.author.id) {
+			api.error('You can\'t kick yourself!')
+			return
+		}
 		if (message.guild.member(user.id).hasPermission("KICK_MEMBERS") && message.author.id !== message.guild.ownerID) {
-				api.error('You can\'t kick staff members unless you are the server owner.')
-				return
-			}
-    args.splice(0,1)
+			api.error('You can\'t kick staff members unless you are the server owner.')
+			return
+		}
+		args.splice(0, 1)
 		var reason
-    if (args[0]) {
-    reason = args.join(' ');
-		user.kick(reason)
-    } else {
-    user.kick()
-    }
+		if (args[0]) {
+			reason = args.join(' ');
+			user.kick(reason)
+		} else {
+			user.kick()
+		}
 		let embed = new Discord.RichEmbed()
 		embed.setTitle('<:apple_boot:372659817287909376> `Kicked ' + user.user.username + '`')
 		embed.setDescription(String.fromCharCode(8203))
