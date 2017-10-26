cb03195896a22b059cf41f6360daa69480ae5c0d
dotJS <justdotJS@icloud.com>
2017-10-26 16:12:22 +0800

    Update how.js

--- modules/prefs/how.js
+++ modules/prefs/how.js
@@ -6,13 +6,13 @@ class howCommand extends Command {
 	constructor() {
 		super({
 			name: 'how',
-			help: 'Show help for using preferences'
+			help: 'Show preferences how to'
 		})
 	}
 
 	hasPermission(message) {
 		if (message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return true
-   // if (message.author.id == message.guild.ownerID) return true
+		// if (message.author.id == message.guild.ownerID) return true
 		return false
 	}
 
@@ -22,20 +22,20 @@ class howCommand extends Command {
 			// Adding a new row to the collection uses `set(key, value)`
 			message.client.settings.set(message.guild.id, message.client.defaultSettings);
 		}
-    
-					let embed = new Discord.RichEmbed()
-					embed.setTitle('<:apple_symbol_question:359559749882085376> `How to use Preferences`')
-					embed.addField('`Get Current Preferences`', 'To get current preferences, just do `.current`. \nMinimum Permission: Kick Members', false)
-          embed.addField('`Setting Preferences`', 'To set a preference, just do `.set preference value`, with `preference` being the name displayed in brackets when showing current preferences, and `value` being the new value you want to set to the preference. \nMinimum Permission: Server Owner', false)
-		
-					embed.setDescription(String.fromCharCode(8203))
-					embed.setColor('#00ff00')
-					//embed.setTimestamp()
-					embed.setFooter('Replying to ' + message.author.tag)
 
-					message.channel.send({
-						embed
-					})
+		let embed = new Discord.RichEmbed()
+		embed.setTitle('<:apple_symbol_question:359559749882085376> `Using Preferences`')
+		embed.addField('`Get Current Preferences`', 'To get current preferences, just do `.current`. \nMinimum Permission: Kick Members', false)
+		embed.addField('`Setting Preferences`', 'To set a preference, just do `.set preference value`, with `preference` being the name displayed in brackets when showing current preferences, and `value` being the new value you want to set to the preference. \nMinimum Permission: Server Owner', false)
+
+		embed.setDescription(String.fromCharCode(8203))
+		embed.setColor('#00ff00')
+		//embed.setTimestamp()
+		embed.setFooter('Replying to ' + message.author.tag)
+
+		message.channel.send({
+			embed
+		})
 
 
 
