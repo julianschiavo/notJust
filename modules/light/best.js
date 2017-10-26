3fdbf92fa16b64f94aace26720a04ca0fe5fb621
dotJS <justdotJS@icloud.com>
2017-10-26 16:26:51 +0800

    Update best.js

--- modules/light/best.js
+++ modules/light/best.js
@@ -10,7 +10,7 @@ class LbestCommand extends Command {
 	}
 
 	hasPermission(message) {
-		if (message.guild.id == '277006003797491712' && message.guild.members.get(message.author.id).roles.has('277031574133014530')) return true
+		if (message.guild && message.guild.id == '277006003797491712' && message.guild.members.get(message.author.id).roles.has('277031574133014530')) return true
 		return false
 	}
 
