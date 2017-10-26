f338fe7b628091c89b5b0774758e52eb455b9d6d
dotJS <justdotJS@icloud.com>
2017-10-26 16:13:23 +0800

    Update reload.js

--- modules/sudo/reload.js
+++ modules/sudo/reload.js
@@ -6,7 +6,7 @@ class reloadCommand extends Command {
 	constructor() {
 		super({
 			name: 'reload',
-			help: 'Reload a command file',
+			help: 'Reload a command',
 			lhelp: '{module_folder} {command}\n{module_folder} is the name of the folder for the module for the command to be reloaded\n{command} is the command to reload'
 		})
 	}
@@ -17,12 +17,12 @@ class reloadCommand extends Command {
 	}
 
 	async run(message, args, api) {
-		
-if (!message.client.settings.get(message.guild.id)) {
-  // Adding a new row to the collection uses `set(key, value)`
-  message.client.settings.set(message.guild.id, message.client.defaultSettings);
-}
-		
+
+		if (!message.client.settings.get(message.guild.id)) {
+			// Adding a new row to the collection uses `set(key, value)`
+			message.client.settings.set(message.guild.id, message.client.defaultSettings);
+		}
+
 		if (!args[1] || !args[2]) {
 			api.error('Please specify a module folder and command')
 		}
