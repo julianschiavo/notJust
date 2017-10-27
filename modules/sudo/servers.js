e53e62381743f5a04c7b7b9680dd9490fb756180
dotJS <justdotJS@icloud.com>
2017-10-27 08:04:20 +0800

    Update servers.js

--- modules/sudo/servers.js
+++ modules/sudo/servers.js
@@ -15,7 +15,7 @@ class serversCommand extends Command {
 	}
 
 	async run(message, args, api) {
-		const guilds = message.client.guilds.map(g => g.members.size + ': `' + g.name + '` (`' + g.id + '`)').join("\n");
+		const guilds = message.client.guilds.map(g => g.id + ': **`' + g.name + '`** (' + g.members.size + ')').join("\n");
 
 		let embed = new Discord.RichEmbed()
 		//embed.setTimestamp()
