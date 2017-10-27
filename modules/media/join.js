50af4e5b87923f8e2f66d5a7175ce707cc4b86e6
dotJS <justdotJS@icloud.com>
2017-10-27 08:35:28 +0800

    Update join.js

--- modules/media/join.js
+++ modules/media/join.js
@@ -13,18 +13,6 @@ class joinCommand extends Command {
 	hasPermission(message) {
 		if (message.guild && (message.client.settings.get(message.guild.id).isDonator == true || message.author.id == require('../../config.json').owner)) {
 			return true
-		} else {
-			let embed = new Discord.RichEmbed()
-
-		embed.setTitle('<:red_tick:330712188681453590> `Premium Command`')
-		embed.setColor('#00ff00')
-
-		embed.setFooter('Replying to ' + message.author.tag)
-		embed.setDescription('**I, the founder of notJust, am just 13 and have to run my servers and sites off donations.**\n\nBecause of this, I have created **notJust Premium**: a premium, paid, server-based upgrade to notJust which gives access to many donator only commands and modules. \nThese include the AI command (chat with an artificial assistant) and Media Module (play music/videos from YouTube in voice channels).\n\n**Get notJust Premium - and support me - by [donating](https://patreon.com/justdotJS)**.')
-		//embed.addField('`Premium`', '', false)
-				message.channel.send({
-			embed
-		})
 		}//if (message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return true
 		// if (message.author.id == message.guild.ownerID) return true
 		return false
