1542277a15cd6cb95fca36d9cb1214ede3d52902
dotJS <justdotJS@icloud.com>
2017-10-30 09:32:06 +0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ const config = require('../config.json')
 exports.run = (bot) => {
   const snekfetch = require('snekfetch')
   snekfetch.post(`https://discordbots.org/api/bots/${bot.user.id}/stats`)
-  .set('Authorization', config.dbotstoken)
+  .set('Authorization', config.apiKeys.dbots)
   .send({ server_count: bot.guilds.size })
   .then(() => console.log('Updated discordbots.org stats.'))
   .catch(err => console.error(`Whoops something went wrong: ${err.body}`));
