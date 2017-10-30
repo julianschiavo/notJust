dc597964f451a264e634624bb3a347457f989b68
dotJS <justdotJS@icloud.com>
2017-10-30 09:37:12 +0800

    Update magic.js

--- modules/fun/magic.js
+++ modules/fun/magic.js
@@ -13,43 +13,61 @@ class ballCommand extends Command {
 		//message.delete()
 		//api.evalembed([245, 236, 71], "🏓 `Ping Successful`", "Heartbeat", one, "Ping", two)
 		var sayings = ["It is certain",
-			"It is decidedly so",
 			"Without a doubt",
-			"Yes, definitely",
-			"You may rely on it",
-			"As I see it, yes",
-			"Most likely",
+			"Definitely",
+			"Rely on it",
+			"TBH, yes",
+			"Probably",
 			"Outlook good",
-			"Yes",
 			"Signs point to yes",
-			"Reply hazy try again",
 			"Ask again later",
-			"Better not tell you now",
 			"Cannot predict now",
 			"Concentrate and ask again",
-			"Don't count on it",
-			"My reply is no",
+			"Hell no",
+			"Nahhhh",
 			"My sources say no",
 			"Outlook not so good",
 			"Very doubtful"
 		];
+		var Ysayings = ["It is certain",
+			"Without a doubt",
+			"Definitely",
+			"Rely on it",
+			"TBH, yes",
+			"Probably",
+			"Outlook good",
+			"Signs point to yes"
+		];
 
+		
+
+		if (message.author.id !== message.guild.ownerID) {
 		var result = Math.floor((Math.random() * sayings.length) + 0);
 		args.splice(0, 1)
 		var q = args.join(' ');
-		//bot.reply(message, sayings[result]);
 		let embed = new Discord.RichEmbed()
-		//embed.setTimestamp()
 		embed.setTitle('<:apple_billiards:372006754965389312> `' + q + '`')
 		embed.setDescription(String.fromCharCode(8203))
 		embed.setColor('#00ff00')
-		//embed.setFooter('Replying to: ' + this.message.author.tag)
-		//embed.setTimestamp()
 		embed.setFooter('Replying to ' + message.author.tag)
 		embed.addField('`Response`', sayings[result], false)
 		message.channel.send({
 			embed
 		})
+		} else {
+			var result = Math.floor((Math.random() * Ysayings.length) + 0);
+		args.splice(0, 1)
+		var q = args.join(' ');
+			let embed = new Discord.RichEmbed()
+		embed.setTitle('<:apple_billiards:372006754965389312> `' + q + '`')
+		embed.setDescription(String.fromCharCode(8203))
+		embed.setColor('#00ff00')
+		embed.setFooter('Replying to ' + message.author.tag)
+		embed.addField('`Response`', Ysayings[result], false)
+		message.channel.send({
+			embed
+		})
+		}
 		return true
 	}
 }
