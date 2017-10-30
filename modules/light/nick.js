7bbc652cb755ccee097aa62c8307d7b95f95c58e
dotJS <justdotJS@icloud.com>
2017-10-30 21:51:53 +0800

    Update nick.js

--- modules/light/nick.js
+++ modules/light/nick.js
@@ -32,15 +32,16 @@ class LnickCommand extends Command {
 		var matcher4 = "-";
 		var matcher5 = "(";
 		var matcher6 = ")";
+		var matcher7 = "0";
 
 		nick = args[0]
 
-		if (nick.indexOf(matcher1) !== -1 | nick.indexOf(matcher2) !== -1) {
+		if (nick.indexOf(matcher1) !== -1 || nick.indexOf(matcher2) !== -1) {
 			api.error('Please don\'t include symbols like UTC or GMT in your nickname.')
 			return;
 		}
 
-		if (nick.indexOf(matcher3) !== -1 | nick.indexOf(matcher4) !== -1 | nick.indexOf(matcher5) !== -1 | nick.indexOf(matcher6) !== -1) {
+		if (nick.indexOf(matcher3) !== -1 || nick.indexOf(matcher4) !== -1 || nick.indexOf(matcher5) !== -1 || nick.indexOf(matcher6) !== -1) {
 			api.error('Please don\'t include `+`/`-`/`(`/`)` in your nickname.')
 			return;
 		}
@@ -51,11 +52,11 @@ class LnickCommand extends Command {
 
 
 		if (timezone.indexOf(matcher1) !== -1 | timezone.indexOf(matcher2) !== -1 | timezone.indexOf(matcher5) !== -1 | timezone.indexOf(matcher6) !== -1) {
-			api.error('Please don\'t include symbols like UTC or GMT or `+`/`-`/`(`/`)` in your timezone setting, just specify the UTC +/- time.')
+			api.error('Please don\'t include symbols like UTC or GMT or `(`/`)` in your timezone setting, just specify the UTC +/- time.')
 			return;
 		}
 
-		if (timezone.indexOf(matcher3) == -1 && timezone.indexOf(matcher4) == -1) {
+		if (timezone.indexOf(matcher3) == -1 && timezone.indexOf(matcher4) == -1 && timezone.indexOf(matcher7) == -1) {
 			api.error('Please include +, - and the timezone time number.')
 			return;
 		}
