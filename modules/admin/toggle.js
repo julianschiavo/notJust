285fb0cd1d987edc474ada32089db612cf6626bc
dotJS <justdotJS@icloud.com>
2017-10-30 20:51:01 +0800

    Create toggle.js

---
+++ modules/admin/toggle.js
@@ -0,0 +1,63 @@
+const Discord = require('discord.js');
+const Command = require('../../cmdModule/commands').Command
+
+class toggleCommand extends Command {
+	constructor() {
+		super({
+			name: 'toggle',
+			help: 'Toggle a self role',
+			lhelp: '{role}\n{role} is the role name or ID.'
+		})
+	}
+
+	hasPermission(message) {
+		//if (message.author.id == require('../../config.json').owner) return true
+		if (message.guild) return true
+		return false
+	}
+
+	async run(message, args, api) {
+		args.splice(0, 1)
+		if (!args[0]) {
+			api.error('Please specify which role to join.')
+			return;
+		}
+		var arg = args.join(' ')
+		var user = message.guild.member(message.author.id)
+
+		var role
+
+		function isN(num) {
+			return !isNaN(num)
+		}
+    
+		if (!isN(args[0])) {
+			role = message.guild.roles.find("name", arg)
+		} else {
+			role = message.guild.roles.get(arg)
+		}
+		if (!role) {
+			api.error('The specified role doesn\'t exist or couldn\'t be found. Try again, and remember to specify the role ID if it\'s name contains more than one word.')
+			return;
+		}
+		args.splice(0, 1)
+
+		if (user.roles.has(role.id)) {
+				user.removeRole(role)
+		} else {
+				user.addRole(role)
+		}
+		let embed = new Discord.RichEmbed()
+		embed.setTitle('<:apple_key:359560553431171084> `Toggled ' + role.name + ' for ' + user.user.username + '`')
+		embed.setDescription(String.fromCharCode(8203))
+		embed.setColor('#00ff00')
+		embed.setFooter('Replying to ' + message.author.tag)
+
+		message.channel.send({
+			embed
+		})
+		return true
+	}
+}
+
+module.exports = toggleCommand
