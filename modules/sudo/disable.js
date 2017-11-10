4c48f6f56a32e92c97bca776d9079e4906890bdd
dotJS <justdotJS@icloud.com>
2017-11-10 20:41:49 +0800

    Create disable.js

---
+++ modules/sudo/disable.js
@@ -0,0 +1,47 @@
+const Discord = require('discord.js');
+
+const Command = require('../../cmdModule/commands').Command
+
+class diCommand extends Command {
+  constructor() {
+    super({
+      name: 'Disable',
+      help: 'Disable a module globally',
+      lhelp: '{name}\n{name} is the code name of the module to disable'
+    })
+  }
+
+  hasPermission(message) {
+    if (message.author.id == require('../../config.json').owner) return true
+    return false
+  }
+
+  async run(message, args, api) {
+    if (!args[1]) {
+      api.error('Please specify a module name to disable.')
+    }
+    var module = args[1].toLowerCase()
+    if (module.indexOf('sudo') > -1) {
+    api.error('You cannot disable the Sudo module, as that would make it impossible to run this command.')
+    }
+
+    const conf = message.client.settings.get('global');
+    conf.disabledModules.push(module)
+    message.client.settings.set('global', conf);
+
+    let embed = new Discord.RichEmbed()
+      embed.setTitle('<:apple_hammer:359560554479878144> `Disabled ' + module + '`')
+    embed.setDescription(String.fromCharCode(8203))
+    embed.setColor('#00ff00')
+    //embed.setTimestamp()
+    embed.setFooter('Replying to ' + message.author.tag)
+
+    message.channel.send({
+      embed
+    })
+
+    return true
+  }
+}
+
+module.exports = diCommand
