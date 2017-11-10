d9f86f9cbf6be53c4bd7a9b8d05f7ba74ad9d24f
dotJS <justdotJS@icloud.com>
2017-11-10 20:43:47 +0800

    Create enable.js

---
+++ modules/sudo/enable.js
@@ -0,0 +1,48 @@
+const Discord = require('discord.js');
+
+const Command = require('../../cmdModule/commands').Command
+
+class enCommand extends Command {
+  constructor() {
+    super({
+      name: 'enable',
+      help: 'Enable a disabled module',
+      lhelp: '{name}\n{name} is the code name of the module to enable'
+    })
+  }
+
+  hasPermission(message) {
+    if (message.author.id == require('../../config.json').owner) return true
+    return false
+  }
+
+  async run(message, args, api) {
+    if (!args[1]) {
+      api.error('Please specify a module name to enable.')
+    }
+    var module = args[1].toLowerCase()
+
+    const conf = message.client.settings.get('global');
+    for (var i=conf.disabledModules.length-1; i>=0; i--) {
+    if (conf.disabledModules[i] === module) {
+        conf.disabledModules.splice(i, 1);
+    }
+}
+    message.client.settings.set('global', conf);
+
+    let embed = new Discord.RichEmbed()
+      embed.setTitle('<:apple_hammer:359560554479878144> `Enabled ' + module + '`')
+    embed.setDescription(String.fromCharCode(8203))
+    embed.setColor('#00ff00')
+    //embed.setTimestamp()
+    embed.setFooter('Replying to ' + message.author.tag)
+
+    message.channel.send({
+      embed
+    })
+
+    return true
+  }
+}
+
+module.exports = enCommand
