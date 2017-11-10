9bfc0839cd8a8193c0cba9f87e13f13e6f0076a1
dotJS <justdotJS@icloud.com>
2017-11-10 14:56:32 +0800

    Update premium.js

--- modules/utility/premium.js
+++ modules/utility/premium.js
@@ -10,9 +10,8 @@ class preCommand extends Command {
   }
 
   hasPermission(message) {
-    var perms = ('../../other/perms')
-    var checkc = check('utility','premium',message)
-    if ((message.guild && message.client.settings.get(message.guild.id).isDonator == true) || checkc) return false
+    var check = ('../../other/perms').check('utility','premium',message)
+    if ((message.guild && message.client.settings.get(message.guild.id).isDonator == true) || check) return false
     //if (message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return true
     // if (message.author.id == message.guild.ownerID) return true
     return true
