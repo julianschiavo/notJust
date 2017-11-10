b9d331830816e18ba49b6028c09c3959f4957687
dotJS <justdotJS@icloud.com>
2017-11-11 07:26:31 +0800

    Update perms.js

--- cmdModule/perms.js
+++ cmdModule/perms.js
@@ -4,27 +4,12 @@ module.exports = {
 			return true
 	}
   },
-  moduleCheck: function (config,command) {
+  moduleCheck: function (config,mod) {
     /*if (config.indexOf(id) > -1) {
 			return true
 	}*/
-	  var modulename
-	  const folders = ['admin','code','fun','light','media','prefs','sudo','utility']
-	  const folderinitial = '../../modules/'
-const fs = require('fs');
-	  //while
-	  for (var i=folders.length-1; i>=0; i--) {
-	  var cfolder = folderinitial + folders[i]
-	  var cfoldern = folders[i]
-fs.readdirSync(cfolder).forEach(file => {
-  if (file.indexOf(command) > -1) {
-	  modulename = cfoldern
-  }
-})
-	  }
-	  if (config.indexOf(modulename) > -1) {
+	  if (config.indexOf(mod) > -1) {
 		  return true
 	  }
-	  //end while
   }
 };
