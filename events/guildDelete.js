2ac43bd602cdca55e3197b49c087cccfce1247a6
dotJS <justdotJS@icloud.com>
2017-11-12 07:32:15 +0800

    Update guildDelete.js

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -2,7 +2,6 @@ const snekfetch = require('snekfetch')
 const config = require('../config.json')
 
 exports.run = (bot) => {
-console.log('hi')
   snekfetch.post(`https://discordbots.org/api/bots/${bot.user.id}/stats`)
     .set('Authorization', config.apiKeys.dbots)
     .send({
