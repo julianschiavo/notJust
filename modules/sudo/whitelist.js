8e5b356d03e468a3dc9c798b595b6763c4b901ad
dotJS <justdotJS@icloud.com>
2017-11-11 20:10:33 +0800

    Update whitelist.js

--- modules/sudo/whitelist.js
+++ modules/sudo/whitelist.js
@@ -33,39 +33,9 @@ class wlCommand extends Command {
       api.error('Please specify an all numeric user or server ID.')
       return
     }
-    if (message.client.guilds.get(args[1])) {
-    var guild = message.client.guilds.get(args[1])
-    var gid
-    if (guild) {
-      gid = guild.id
-    } else {
-      gid = args[1]
-    }
 
-    if (!message.client.settings.get(gid)) {
-      message.client.settings.set(gid, message.client.defaultSettings);
-    }
     
-
-    const thisConf = message.client.settings.get(gid);
-    thisConf.isBlacklisted = false;
-    message.client.settings.set(gid, thisConf);
-
-    let embed = new Discord.RichEmbed()
-    if (guild) {
-      embed.setTitle('<:apple_hand_wave:359559674099400704> `Whitelisted ' + guild.name + '!`')
-    } else {
-      embed.setTitle('<:apple_hand_wave:359559674099400704> `Whitelisted ' + gid + '!`')
-    }
-    embed.setDescription(String.fromCharCode(8203))
-    embed.setColor('#00ff00')
-    //embed.setTimestamp()
-    embed.setFooter('Replying to ' + message.author.tag)
-
-    message.channel.send({
-      embed
-    })
-    } else if (message.client.users.get(args[1])) {
+    if (message.client.users.get(args[1])) {
     var user = message.client.users.get(args[1])
     var current = message.client.settings.get('global')
 
@@ -88,10 +58,39 @@ for (var i=current.blacklistedUsers.length-1; i>=0; i--) {
     message.channel.send({
       embed
     })
+    } else {
+      var guild = message.client.guilds.get(args[1])
+    var gid
+    if (guild) {
+      gid = guild.id
+    } else {
+      gid = args[1]
+    }
+      if (!message.client.settings.get(gid)) {
+      message.client.settings.set(gid, message.client.defaultSettings);
+    }
     
-  } else {
-    api.error('User/server could not be found. Remember that if you are whitelisting a user, he must be in one of the bot\'s servers.')
-  }
+
+    const thisConf = message.client.settings.get(gid);
+    thisConf.isBlacklisted = false;
+    message.client.settings.set(gid, thisConf);
+
+    let embed = new Discord.RichEmbed()
+    if (guild) {
+      embed.setTitle('<:apple_hand_wave:359559674099400704> `Whitelisted ' + guild.name + '!`')
+    } else {
+      embed.setTitle('<:apple_hand_wave:359559674099400704> `Whitelisted ' + gid + '!`')
+    }
+    embed.setDescription(String.fromCharCode(8203))
+    embed.setColor('#00ff00')
+    //embed.setTimestamp()
+    embed.setFooter('Replying to ' + message.author.tag)
+
+    message.channel.send({
+      embed
+    })
+    }
+      
     return true
   }
 }
