b2a7cf7a8833c4479ab8946685ca69dc8efe8b13
dotJS <justdotJS@icloud.com>
2017-11-12 07:32:02 +0800

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -2,7 +2,6 @@ const snekfetch = require('snekfetch')
 const config = require('../config.json')
 
 exports.run = (bot, guild) => {
-console.log('higuildcreate')
  snekfetch.post(`https://discordbots.org/api/bots/${bot.user.id}/stats`)
     .set('Authorization', config.apiKeys.dbots)
     .send({
