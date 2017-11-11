629bd29b8b191afff6be71c7c5bc06150ca71e1b
dotJS <justdotJS@icloud.com>
2017-11-11 14:38:49 +0800

    Update join.js

--- modules/sudo/join.js
+++ modules/sudo/join.js
@@ -24,6 +24,7 @@ class joinCommand extends Command {
     }
     if (!isN(args[1])) {
       api.error('Please specify an all numeric guild id.')
+      return
     }
     var guild = message.client.guilds.get(args[1])
     var chan = guild.channels.array()[0];
