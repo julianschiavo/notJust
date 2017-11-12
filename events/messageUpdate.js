8bf343ab0d2feb5e5f752bb63d4f6bba4767969e
dotJS <justdotJS@icloud.com>
2017-11-12 16:43:22 +0800

    Create messageUpdate.js

---
+++ events/messageUpdate.js
@@ -0,0 +1,34 @@
+const config = require('../config.json')
+const ta = require('../timeago.js')
+const Discord = require('discord.js')
+
+exports.run = (bot, omessage, nmessage) => {
+if (nmessage.system == true) {
+return
+}
+ var conf = bot.settings.get(nmessage.guild.id)
+  if (conf) {
+  
+  if (conf.useLogs && conf.useLogs == true) {
+   var channel
+			if (nmessage.guild.channels.find('name', conf.logChannel)) {
+				var channel = nmessage.guild.channels.find('name', conf.logChannel)
+			} else {
+				console.log('Failed to find logChannel for ' + nmessage.guild.name)
+				return
+			}
+  
+	  channel.send('<:apple_pencil_paper:359560552701231106> message `' + nmessage.id + '` edited from\n```' + omessage.content + '```\nto\n```' + nmessage.content + '``` by `' + message.author.tag + '`')
+      
+  } else {
+  return
+  }
+  
+  } else {
+    bot.settings.set(nmessage.guild.id, bot.defaultSettings);
+    return
+  }
+  
+  
+  
+  }
