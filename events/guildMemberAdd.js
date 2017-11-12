a4bd8e4c6b0231437f5128e28b149c0d79d2c11a
dotJS <justdotJS@icloud.com>
2017-11-12 15:51:58 +0800

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,34 +3,21 @@ const ta = require('../timeago.js')
 const Discord = require('discord.js')
 
 exports.run = (bot, member) => {
+   var user = member
  var conf = bot.settings.get(member.guild.id)
   if (conf) {
   
   if (conf.useLogs && conf.useLogs == true) {
-  
-  var user = member
-      var icon = user.user.avatarURL
-      var time = ta.ago(user.user.createdTimestamp);
-      var id = user.user.id
-      var join = ta.ago(user.joinedTimestamp);
-
-      var name = user.user.username
-      let embed = new Discord.RichEmbed()
-      embed.setTitle('<:apple_symbol_info:359559750096257024> `' + name + ' Joined`')
-      embed.setColor('#00ff00')
-      embed.setTimestamp()
-      embed.setDescription(String.fromCharCode(8203))
-      embed.setThumbnail(icon)
-      embed.addField('`ID`', id, false)
-      embed.addField('`Created`', time, true)
-      embed.addField('`Joined`', join, true)
-      if (member.guild.channels.find('name', conf.logChannel)) {
-       member.guild.channels.find('name', conf.logChannel).send({
-        embed
-      })
-      } else {
-       console.log('Failed to find logChannel for ' + member.guild.name)
-      }
+  //<:apple_person_raising_hand:359559750712557568>
+   var channel
+			if (member.guild.channels.find('name', conf.logChannel)) {
+				var channel = member.guild.channels.find('name', conf.logChannel)
+			} else {
+				console.log('Failed to find logChannel for ' + member.guild.name)
+				return
+			}
+   
+  channel.send('<:apple_person_raising_hand:359559750712557568> `' + user.user.tag + '` (`' + user.id + '`) joined (created ' + ta.ago(user.user.createdTimestamp) + ')')
       
   } else {
   return
