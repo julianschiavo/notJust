8abec373e73d0f4dfb01c358525eaac851cebbfe
dotJS <justdotJS@icloud.com>
2017-11-12 16:13:48 +0800

    Create emojiUpdate.js

---
+++ events/emojiUpdate.js
@@ -0,0 +1,33 @@
+const config = require('../config.json')
+const ta = require('../timeago.js')
+const Discord = require('discord.js')
+
+exports.run = (bot, oemoji, nemoji) => {
+ var conf = bot.settings.get(nemoji.guild.id)
+  if (conf) {
+  
+  if (conf.useLogs && conf.useLogs == true) {
+   var channel
+			if (nemoji.guild.channels.find('name', conf.logChannel)) {
+				var channel = nemoji.guild.channels.find('name', conf.logChannel)
+			} else {
+				console.log('Failed to find logChannel for ' + nemoji.guild.name)
+				return
+			}
+   
+   if (nemoji.name !== oemoji.name) {
+  channel.send('<:apple_face_smile:359559572634992672> emoji `' + nemoji.id + '` changed name from `' + oemoji.name + '` to `' + nemoji.name + '`')
+  }
+      
+  } else {
+  return
+  }
+  
+  } else {
+    bot.settings.set(emoji.guild.id, bot.defaultSettings);
+    return
+  }
+  
+  
+  
+  }
