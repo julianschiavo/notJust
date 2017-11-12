30ebcfe3fc725ee8598f630d77a44f1afdd820dd
dotJS <justdotJS@icloud.com>
2017-11-12 16:49:21 +0800

    Update messageDelete.js

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -4,22 +4,24 @@ const Discord = require('discord.js')
 
 exports.run = (bot, message) => {
 if (message.system == true) {
+	console.log('a')
 return
 }
  var conf = bot.settings.get(message.guild.id)
   if (conf) {
-  
+  console.log('a')
   if (conf.useLogs && conf.useLogs == true) {
-   var channel
+   console.log('a')
+	  var channel
 			if (message.guild.channels.find('name', conf.logChannel)) {
 				var channel = message.guild.channels.find('name', conf.logChannel)
 			} else {
 				console.log('Failed to find logChannel for ' + message.guild.name)
 				return
 			}
-  
+  console.log('a')
 	  channel.send('<:apple_pencil_paper:359560552701231106> message `' + message.id + '`\n```' + message.content + '```\ndeleted by `' + message.author.tag + '`')
-      
+     console.log('a')
   } else {
   return
   }
