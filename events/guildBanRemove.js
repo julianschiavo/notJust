0e3b5cc611d3fc489db30ca0d933890ac6047474
dotJS <justdotJS@icloud.com>
2017-11-12 16:23:14 +0800

    Create guildBanRemove.js

---
+++ events/guildBanRemove.js
@@ -0,0 +1,31 @@
+const config = require('../config.json')
+const ta = require('../timeago.js')
+const Discord = require('discord.js')
+
+exports.run = (bot, guil, user) => {
+ var conf = bot.settings.get(guil.id)
+  if (conf) {
+  
+  if (conf.useLogs && conf.useLogs == true) {
+   var channel
+			if (guil.channels.find('name', conf.logChannel)) {
+				var channel = guil.channels.find('name', conf.logChannel)
+			} else {
+				console.log('Failed to find logChannel for ' + guil.name)
+				return
+			}
+   
+  channel.send('<:apple_hammer:359560554479878144> `' + user.id + '` unbanned')
+      
+  } else {
+  return
+  }
+  
+  } else {
+    bot.settings.set(emoji.guild.id, bot.defaultSettings);
+    return
+  }
+  
+  
+  
+  }
