3bcfdd5824200c6a898ab4ea7dd84e18f1256d21
dotJS <justdotJS@icloud.com>
2017-11-12 16:31:14 +0800

    Update channelUpdate.js

--- events/channelUpdate.js
+++ events/channelUpdate.js
@@ -16,39 +16,39 @@ exports.run = (bot, old, nuu) => {
 
       if (nuu.type == 'text') {
       if (nuu.name !== old.name) {
-      channel.send('<:apple_symbol_hash:359559749785616387> channel `' + nuu.id + '` changed name from `#' + old.name + '` to `#' + nuu.name + '`')
+      channel.send('<:apple_symbol_name_badge:359559749760581633> channel `' + nuu.id + '` changed name from `#' + old.name + '` to `#' + nuu.name + '`')
       }
       
       if ((nuu.topic !== old.topic) && (old.topic == '' || old.topic == null)) {
-      channel.send('<:apple_symbol_hash:359559749785616387> channel <#' + nuu.id + '> added topic\n```' + nuu.topic + '```')
+      channel.send('<:apple_scroll:359560551904444435> channel <#' + nuu.id + '> added topic\n```' + nuu.topic + '```')
       }
       if ((nuu.topic !== old.topic) && (nuu.topic == '' || nuu.topic == null)) {
-      channel.send('<:apple_symbol_hash:359559749785616387> channel <#' + nuu.id + '> removed topic\n```' + old.topic + '```')
+      channel.send('<:apple_scroll:359560551904444435> channel <#' + nuu.id + '> removed topic\n```' + old.topic + '```')
       }
       if (nuu.topic !== old.topic && (old.topic == '' || old.topic == null) && (nuu.topic == '' || nuu.topic == null)) {
-      channel.send('<:apple_symbol_hash:359559749785616387> channel <#' + nuu.id + '> changed topic from\n```' + old.topic + '``` \nto\n ```' + nuu.topic + '```')
+      channel.send('<:apple_scroll:359560551904444435> channel <#' + nuu.id + '> changed topic from\n```' + old.topic + '``` \nto\n ```' + nuu.topic + '```')
       }
       
       if (old.nsfw == true && nuu.nsfw == false) {
-      channel.send('<:apple_symbol_hash:359559749785616387> channel <#' + nuu.id + '> removed NSFW')
+      channel.send('<:apple_symbol_underage:359559749760450561> channel <#' + nuu.id + '> removed NSFW')
       }
       if (old.nsfw == false && nuu.nsfw == true) {
-      channel.send('<:apple_symbol_hash:359559749785616387> channel <#' + nuu.id + '> made NSFW')
+      channel.send('<:apple_symbol_underage:359559749760450561> channel <#' + nuu.id + '> made NSFW')
       }
       } else if (nuu.type == 'voice') {
 
       if (nuu.name !== old.name) {
-      channel.send('<:apple_symbol_hash:359559749785616387> channel `' + nuu.id + '` name changed from `' + old.name + '` to `' + nuu.name + '`')
+      channel.send('<:apple_symbol_name_badge:359559749760581633> channel `' + nuu.id + '` name changed from `' + old.name + '` to `' + nuu.name + '`')
       }
       
       if ((nuu.userLimit !== old.userLimit) && old.userLimit == 0) {
-      channel.send('<:apple_symbol_hash:359559749785616387> channel `' + nuu.name + '` added userlimit `' + nuu.userLimit + '`')
+      channel.send('<:apple_lock:359560553401942017> channel `' + nuu.name + '` added userlimit `' + nuu.userLimit + '`')
       }
       if ((nuu.userLimit !== old.userLimit) && nuu.userLimit == 0) {
-      channel.send('<:apple_symbol_hash:359559749785616387> channel `' + nuu.name + '` removed userlimit')
+      channel.send('<:apple_lock:359560553401942017> channel `' + nuu.name + '` removed userlimit')
       }
       if ((nuu.userLimit !== old.userLimit) && (nuu.userLimit !== 0 && old.userLimit !== 0)) {
-      channel.send('<:apple_symbol_hash:359559749785616387> channel `' + nuu.name + '` changed userlimit from `' + old.userLimit + '` to `' + nuu.userLimit + '`')
+      channel.send('<:apple_lock:359560553401942017> channel `' + nuu.name + '` changed userlimit from `' + old.userLimit + '` to `' + nuu.userLimit + '`')
       }
       
       }
