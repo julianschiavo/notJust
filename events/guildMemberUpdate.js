5756de5a983597726a0fe38c1479a45b76f44ee9
dotJS <justdotJS@icloud.com>
2017-11-12 10:04:16 +0800

    Update guildMemberUpdate.js

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -2,7 +2,7 @@ const config = require('../config.json')
 const ta = require('../timeago.js')
 const Discord = require('discord.js')
 
-exports.run = (bot, member) => {
+exports.run = (bot, member, oser) => {
  var conf = bot.settings.get(member.guild.id)
   if (conf) {
   
@@ -10,18 +10,58 @@ exports.run = (bot, member) => {
   
   var user = member
       var icon = user.user.avatarURL
-      var time = ta.ago(user.user.createdTimestamp);
+      var oicon = oser.user.avatarURL
+      
       var id = user.user.id
-      var join = ta.ago(user.joinedTimestamp);
 
       var name = user.user.username
+      var oname = oser.user.username
+      
+      var nick = user.nickname
+      var onick = oser.nickname
+      
+      var roles = user.roles.map(r => r.name).join('`, `')
+   var oroles = oser.roles.map(r => r.name).join('`, `')
+      
+      
       let embed = new Discord.RichEmbed()
-      embed.setTitle('<:apple_symbol_info:359559750096257024> `' + name + ' Updated`')
+      
       embed.setColor('#00ff00')
       embed.setTimestamp()
       embed.setDescription(String.fromCharCode(8203))
-      embed.setThumbnail(icon)
       embed.addField('`ID`', id, false)
+   
+   if (icon !== oicon) {
+    embed.setTitle('<:apple_symbol_info:359559750096257024> `' + name + ' Changed Avatar`')
+    embed.setThumbnail(icon)
+   } else {
+    
+   if (name !== oname) {
+    embed.setTitle('<:apple_symbol_info:359559750096257024> `' + name + ' Changed Username`')
+    embed.addField('`Old Username`', oname, true)
+    embed.addField('`New Username`', name, true)
+   } else {
+    
+  if (roles !== oroles) {
+    embed.setTitle('<:apple_symbol_info:359559750096257024> `' + name + ' Changed Roles`')
+    embed.addField('`Old Roles`', roles, false)
+    embed.addField('`New Roles`', roles, false)
+   } else {
+    
+    if (nick !== onick) {
+    embed.setTitle('<:apple_symbol_info:359559750096257024> `' + name + ' Changed Nickname`')
+    embed.addField('`Old Nickname`', onick, false)
+    embed.addField('`New Nickname`', nick, false)
+   } else {
+    embed.setTitle('<:apple_symbol_info:359559750096257024> `' + name + ' Changed`')
+   }
+    
+   }
+    
+   }
+    
+   }
+      
       /*embed.addField('`Created`', time, true)
       embed.addField('`Joined`', join, true)*/
       if (member.guild.channels.find('name', conf.logChannel)) {
