76a5b5d723367a262e868f3b67ad8206f7022f00
dotJS <justdotJS@icloud.com>
2017-11-24 09:19:23 +0800

    Update start.js

--- modules/currency/start.js
+++ modules/currency/start.js
@@ -39,7 +39,7 @@ class startCommand extends Command {
     var chan2 = message.guild.channels.find('name', 'invites')
     var inv = await chan.createInvite({maxAge:0,unique:true},'Gold Bar Invite for ' + message.author.tag)
     
-    chan2.send('<:gold:383074743089364992> Created **Gold Bar Invite** `' + inv.replace('https://discord.gg/', '') + '` for `' + message.author.tag + '`')
+    chan2.send('<:gold:383074743089364992> Created **Gold Bar Invite** `' + inv.toString().replace('https://discord.gg/', '') + '` for `' + message.author.tag + '`')
 
       let embed = new Discord.RichEmbed()
       embed.setTitle('<:gold:383074743089364992> `Gold Bar Invite Created`')
