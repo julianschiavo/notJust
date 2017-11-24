bad3f82fa54dd99d3699da60165771a93f7149a1
dotJS <justdotJS@icloud.com>
2017-11-24 12:04:58 +0800

    Create check.js

---
+++ modules/currency/check.js
@@ -0,0 +1,57 @@
+const Discord = require('discord.js');
+const Command = require('../../cmdModule/commands').Command
+
+class checkCommand extends Command {
+  constructor() {
+    super({
+      name: 'check',
+      help: 'Check your gold bar status'
+    })
+  }
+
+  hasPermission(message) {
+   if (message.guild && message.guild.id == '268970339948691456') return true
+    return false
+  }
+
+  async run(message, args, api) {
+  
+   var user
+      if (args[1]) {
+        user = message.guild.member(message.mentions.users.first());
+      } else {
+        user = message.guild.member(message.author);
+      }
+
+    if (!message.client.currency.get(user.id)) {
+      var curr = {
+      amount: 0,
+      invCode: ''
+      }
+      message.client.currency.set(user.id, curr);
+    }
+    var inv = message.client.currency.get(user.id).invCode
+      var curr = message.client.currency.get(user.id)
+    
+      let embed = new Discord.RichEmbed()
+      embed.setTitle('<:gold:383074743089364992> `Gold Bar Status`')
+   embed.addField('Amount', curr.amount, true)
+   embed.addField('Invite', '`' + inv.toString() + '`', true)
+    
+      embed.setColor('#00ff00')
+      embed.setFooter('Replying to ' + message.author.tag)
+      embed.setDescription(String.fromCharCode(8203))
+      message.channel.send({
+        embed
+      })
+
+
+
+
+
+
+    return true
+  }
+}
+
+module.exports = checkCommand
