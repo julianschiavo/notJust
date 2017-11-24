046f8d736fe80bd049f8555fa05957daa19d07f1
dotJS <justdotJS@icloud.com>
2017-11-24 09:09:10 +0800

    Update start.js

--- modules/currency/start.js
+++ modules/currency/start.js
@@ -18,13 +18,13 @@ class startCommand extends Command {
   async run(message, args, api) {
     var chan = message.guild.channels.find('name', 'welcome')
     var chan2 = message.guild.channels.find('name', 'invites')
-    var inv = chan.createInvite({maxAge:0,unique:true},'Gold Bar Invite for ' + message.author.user.tag)
+    var inv = chan.createInvite({maxAge:0,unique:true},'Gold Bar Invite for ' + message.author.tag)
     
-    chan2.send('<:gold:383074743089364992> Created **Gold Bar Invite** for' + message.author.user.tag)
+    chan2.send('<:gold:383074743089364992> Created **Gold Bar Invite** for' + message.author.tag)
 
       let embed = new Discord.RichEmbed()
       embed.setTitle('<:gold:383074743089364992> `Gold Bar Invite Created`')
-   embed.addField('User', message.author.user.tag, true)
+   embed.addField('User', message.author.tag, true)
    embed.addField('Invite', inv.toString(), true)
     
       embed.setColor('#00ff00')
