59a1956b3a5a11da4d22eebb163211bdbb0e5a54
dotJS <justdotJS@icloud.com>
2017-11-24 09:17:34 +0800

    Update start.js

--- modules/currency/start.js
+++ modules/currency/start.js
@@ -16,11 +16,30 @@ class startCommand extends Command {
   }
 
   async run(message, args, api) {
+    
+    var user = message.guild.member(message.author);
+    if (!message.client.currency.get(user.id)) {
+      var curr = {
+      amount: 0,
+      invite: false
+      }
+      message.client.currency.set(user.id, curr);
+    }
+    if ((message.client.currency.get(user.id).invite == true) && user.id !== '193908323911860224') {
+      api.error('You have already created an invite. Find it in <#383422802407194625>.')
+      return
+    } else {
+      var curr = message.client.currency.get(user.id)
+      curr.invite = true
+      message.client.currency.set(user.id,curr)
+    }
+    
+    
     var chan = message.guild.channels.find('name', 'welcome')
     var chan2 = message.guild.channels.find('name', 'invites')
     var inv = await chan.createInvite({maxAge:0,unique:true},'Gold Bar Invite for ' + message.author.tag)
     
-    chan2.send('<:gold:383074743089364992> Created **Gold Bar Invite** for `' + message.author.tag + '`: `' + inv + '`')
+    chan2.send('<:gold:383074743089364992> Created **Gold Bar Invite** `' + inv.replace('https://discord.gg/', '') + '` for `' + message.author.tag + '`')
 
       let embed = new Discord.RichEmbed()
       embed.setTitle('<:gold:383074743089364992> `Gold Bar Invite Created`')
