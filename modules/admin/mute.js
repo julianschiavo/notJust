be6471fb5a10add0925726886a38afef07f547c2
dotJS <justdotJS@icloud.com>
2017-12-04 08:57:08 +0800

    Update mute.js

--- modules/admin/mute.js
+++ modules/admin/mute.js
@@ -6,13 +6,13 @@ class muteCommand extends Command {
     super({
       name: 'mute',
       help: 'Mute a member',
-      lhelp: '{user} [reason]\n{user} is the user to mute (id or mention)\n[reason] is the Audit Log reason for the mute'
+      lhelp: '{user} [reason]\n{user} is the user to mute\n[reason] is the Audit Log reason for the mute'
     })
   }
 
   hasPermission(message) {
     //if (message.author.id == require('../../config.json').owner) return true
-    if (message.guild && message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return true
+    if (message.guild && message.guild.member(message.author).hasPermission("KICK_MEMBERS") && message.guild.member('329772339967426560').hasPermission("KICK_MEMBERS")) return true
     return false
   }
 
@@ -26,7 +26,7 @@ class muteCommand extends Command {
     var arg = args[0]
     var user = api.getUser(arg, 'member')
     if (user.user.id == message.author.id) {
-      api.error('You can\'t mute yourself!')
+      api.error('You can\'t mute yourself.')
       return
     }
     args.splice(0, 1)
@@ -48,23 +48,28 @@ class muteCommand extends Command {
       var reason
       if (args[0]) {
         reason = args.join(' ');
-        user.addRole(role, reason)
+        user.addRole(role, reason).then(promise => success()).catch(err => {
+          api.error(err)
+        })
       } else {
-        user.addRole(role)
+        user.addRole(role).then(promise => success()).catch(err => {
+          api.error(err)
+        })
       }
-      let embed = new Discord.RichEmbed()
-      embed.setTitle('<:apple_muted:372902540393709569> `Muted ' + user.user.username + '`')
-      embed.setDescription(String.fromCharCode(8203))
-      embed.setColor('#00ff00')
-      if (reason) {
-        embed.addField('`Reason`', reason, false)
-      }
-      //embed.setTimestamp()
-      embed.setFooter('Replying to ' + message.author.tag)
 
-      message.channel.send({
-        embed
-      })
+      function success() {
+        let embed = new Discord.RichEmbed()
+        embed.setTitle('<:apple_muted:372902540393709569> `Muted ' + user.user.username + '`')
+        embed.setDescription(String.fromCharCode(8203))
+        embed.setColor('#00ff00')
+        if (reason) {
+          embed.addField('`Reason`', reason, false)
+        }
+        embed.setFooter('Replying to ' + message.author.tag)
+        message.channel.send({
+          embed
+        })
+      }
     } else {
       api.error('This server\'s mute role is invalid, non existent, or couldn\'t be found. Please check your preferences.')
     }
