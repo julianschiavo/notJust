c5f81e370a0311cd6c292074a1539e971d69979f
dotJS <justdotJS@icloud.com>
2017-12-04 09:01:17 +0800

    Update role.js

--- modules/admin/role.js
+++ modules/admin/role.js
@@ -6,20 +6,20 @@ class roleCommand extends Command {
     super({
       name: 'role',
       help: 'Add/remove a role from a member',
-      lhelp: '{user} {role} [reason]\n{user} is the user to ban (id or mention)\n{role} is the role name or ID. If the role name contains 2 words, you MUST provide the ID.\n[reason] is the Audit Log reason for the role modification\n(Please note it will add or remove the role depending on if the user has it or not)'
+      lhelp: '{user} {role} [reason]\n{user} is the user to add or remove a role to\n{role} is the role name or ID. If the role name contains 2 words, you MUST provide the ID.\n[reason] is the Audit Log reason for the role modification\n(Please note it will add or remove the role depending on if the user has it or not)'
     })
   }
 
   hasPermission(message) {
     //if (message.author.id == require('../../config.json').owner) return true
-    if (message.guild && message.guild.member(message.author).hasPermission("MANAGE_ROLES")) return true
+    if (message.guild && message.guild.member(message.author).hasPermission("MANAGE_ROLES") && message.guild.member('329772339967426560').hasPermission("MANAGE_ROLES")) return true
     return false
   }
 
   async run(message, args, api) {
     args.splice(0, 1)
     if (!args[0]) {
-      api.error('Please specify which user to modify a role for.')
+      api.error('Please specify which user to add/remove a role for.')
       return;
     } else if (!args[1]) {
       api.error('Please specify which role to add/remove.')
@@ -27,10 +27,7 @@ class roleCommand extends Command {
     }
     var arg = args[0]
     var user = api.getUser(arg, 'member')
-    /*if (message.guild.member(user.id).hasPermission("MANAGE_ROLES") && message.author.id !== message.guild.ownerID) {
-    	api.error('You can\'t modify staff member roles unless you are the server owner.')
-    	return
-    }*/
+
     args.splice(0, 1)
     var role
 
@@ -55,32 +52,42 @@ class roleCommand extends Command {
       typ2 = 'from'
       if (args[0]) {
         reason = args.join(' ');
-        user.removeRole(role, reason)
+        user.removeRole(role, reason).then(promise => success()).catch(err => {
+          api.error(err)
+        })
       } else {
-        user.removeRole(role)
+        user.removeRole(role).then(promise => success()).catch(err => {
+          api.error(err)
+        })
       }
     } else {
       type = 'Added'
       typ2 = 'to'
       if (args[0]) {
         reason = args.join(' ');
-        user.addRole(role, reason)
+        user.addRole(role, reason).then(promise => success()).catch(err => {
+          api.error(err)
+        })
       } else {
-        user.addRole(role)
+        user.addRole(role).then(promise => success()).catch(err => {
+          api.error(err)
+        })
       }
     }
-    let embed = new Discord.RichEmbed()
-    embed.setTitle('<:apple_hammer:359560554479878144> `' + type + ' ' + role.name + ' ' + typ2 + ' ' + user.user.username + '`')
-    embed.setDescription(String.fromCharCode(8203))
-    embed.setColor('#00ff00')
-    if (reason) {
-      embed.addField('`Reason`', reason, false)
-    }
-    embed.setFooter('Replying to ' + message.author.tag)
 
-    message.channel.send({
-      embed
-    })
+    function success() {
+      let embed = new Discord.RichEmbed()
+      embed.setTitle('<:apple_hammer:359560554479878144> `' + type + ' ' + role.name + ' ' + typ2 + ' ' + user.user.username + '`')
+      embed.setDescription(String.fromCharCode(8203))
+      embed.setColor('#00ff00')
+      if (reason) {
+        embed.addField('`Reason`', reason, false)
+      }
+      embed.setFooter('Replying to ' + message.author.tag)
+      message.channel.send({
+        embed
+      })
+    }
     return true
   }
 }
