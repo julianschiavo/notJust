a21abc9e51b3049b8fb0667e6eb9bffa6dfff47f
dotJS <justdotJS@icloud.com>
2017-12-08 18:26:46 +0800

    Update ask.js

--- modules/fun/ask.js
+++ modules/fun/ask.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
-const config = require('../../config.json')
+const config = require('../../config.json');
+var changeCase = require('change-case');
 const vdSDK = require("virtual-device-sdk");
 const alexa = new vdSDK.VirtualDevice(config.apiKeys.alexa);
 
