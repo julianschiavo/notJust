746bb0ed13c0429d04a72fad37efb0d45a72cb9d
dotJS <justdotJS@icloud.com>
2017-12-08 18:25:21 +0800

    Update ask.js

--- modules/fun/ask.js
+++ modules/fun/ask.js
@@ -15,24 +15,35 @@ class alCommand extends Command {
   }
 
   hasPermission(message) {
-    if (message.guild && message.guild.member(message.author).id == '193908323911860224' && (message.client.settings.get(message.guild.id).isDonator == true || message.author.id == require('../../config.json').owner)) return true
+    if (message.guild && message.guild.id == '193908323911860224' && (message.client.settings.get(message.guild.id).isDonator == true || message.author.id == require('../../config.json').owner)) return true
     return false
   }
 
   async run(message, args, api) {
     args.splice(0, 1)
     if (!args[0]) {
-      api.error('Please provide some text to send to the AI.')
+      api.error('Please provide some text to ask Alexa.')
       return
     }
     //console.log('hi')
     var arg = args.join(' ');
     message.channel.startTyping();
     alexa.message(arg).then((result) => {
-      message.channel.send(result.transcript).catch(console.error);
+      var text = changeCase.sentenceCase(result.transcript)
+      //message.channel.send(result.transcript).catch(console.error);
+      let embed = new Discord.RichEmbed()
+      embed.setTitle('<:apple_siri:356765237778579457> `' + arg + '`')
+      embed.setDescription(String.fromCharCode(8203))
+      embed.setColor('#00ff00')
+      embed.setFooter('Replying to ' + message.author.tag)
+      embed.addField('`Response`', text, false)
+      embed.addField('`Audio Response`', result.transcriptAudioURL, false)
+      message.channel.send({
+        embed
+      })
       message.channel.stopTyping();
-      console.log("Reply Transcript: " + result.transcript);
-      console.log("Reply Audio: " + result.transcript_audio_url);
+      //console.log("Reply Transcript: " + result.transcript);
+      //console.log("Reply Audio: " + result.transcript_audio_url);
     });
     return true
   }
