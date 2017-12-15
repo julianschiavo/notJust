27a1513b7ecb47ab963607c4bff4b5377472f5c1
dotJS <justdotJS@icloud.com>
2017-12-15 08:38:52 +0800

    Update about.js

--- modules/utility/about.js
+++ modules/utility/about.js
@@ -30,19 +30,20 @@ class aboutCommand extends Command {
     //embed.setTimestamp()
     embed.setFooter('Replying to ' + message.author.tag)
     embed.setDescription('[notJust](https://notjust.xyz) is a leading, state-of-the-art Discord bot founded by dotJS. \nIt has modules which include fun commands, music commands, moderation commands, and more.')
+    embed.addField('`Stats`', 'notJust is on ' + servers + ' servers, which contain ' + channels + ' channels, and ' + users + ' users.', false)
     embed.addBlankField(false)
     embed.addField('`Get`', '**[Get notJust by clicking here.](https://discordapp.com/oauth2/authorize?client_id=329772339967426560&scope=bot&permissions=8)** \nWe highly recommend you give it the "Administrator" permission to ensure all commands work!', false)
-    embed.addBlankField(false)
+    
 
     if (message.guild && (message.client.settings.get(message.guild.id).isDonator == true || message.author.id == require('../../config.json').owner)) {} else {
-      embed.addField('`Premium`', 'notJust Premium is an upgrade to notJust that adds commands and modules. Find out more by doing `.premium` in the chat!', false)
       embed.addBlankField(false)
+      embed.addField('`Premium`', 'notJust Premium is an upgrade to notJust that adds commands and modules. Find out more by doing `.premium` in the chat!', false)
+      //embed.addBlankField(false)
     }
 
 
-    embed.addField('`Stats`', 'notJust is on ' + servers + ' servers, which contain ' + channels + ' channels, and ' + users + ' users.', false)
     //embed.addField('`Description`', 'notJust is a leading, state-of-the-art Discord bot founded by dotJS. \nIt has modules which include fun commands, music commands, moderation commands, and more.', false)		
-    embed.addField('`Contributors`', 'notJust couldn\'t have been built without the assistance of `King - Vlad` and `szymex73`, so thanks for all their help.', false)
+    //embed.addField('`Contributors`', 'notJust couldn\'t have been built without the assistance of `King - Vlad` and `szymex73`, so thanks for all their help.', false)
     message.channel.send({
       embed
     })
