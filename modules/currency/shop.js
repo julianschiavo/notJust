a87f137dbe4044ac04f633ce2267944126a1dd5f
dotJS <justdotJS@icloud.com>
2017-12-20 16:54:25 +0800

    Update shop.js

--- modules/currency/shop.js
+++ modules/currency/shop.js
@@ -19,12 +19,12 @@ class shopCommand extends Command {
       for (var i = 0; i < shop.length; i++) {
               let embed = new Discord.RichEmbed()
       embed.setTitle('<:goldbar:383480100282171392> `' + shop[i].name + '`')
-       embed.addField('ID',shop[i].id,false)
+       embed.addField('ID',shop[i].name,false)
         embed.addField('Available',shop[i].av,true)
         embed.addField('Cost',shop[i].price,true)
       embed.setColor('#00ff00')
       embed.setFooter('Replying to ' + message.author.tag)
-      embed.setDescription(String.fromCharCode(8203))
+      embed.setDescription(shop[i].description + '\n')
       message.channel.send({
         embed
       })
