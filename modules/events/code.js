1fad491ff347a640bab3ba1e1094b67e6ed47980
dotJS <justdotJS@icloud.com>
2018-01-08 14:41:25 +0800

    Update code.js

--- modules/events/code.js
+++ modules/events/code.js
@@ -25,21 +25,21 @@ message.guild.channels.find('name','code-revived').send({embed})
     
     embed = new Discord.RichEmbed();
 embed.setColor(" #ffff00 ");
-embed.setTitle("🌞 `#Code Revived`");
+embed.setTitle("<:code:391965427842023424> `#Code Revived`");
 embed.setDescription("It's been a while, but I'm happy to finally announce #Code Revived, an event rising from the ashes of #Code, which was shut down in 2017.");
     embed.addBlankField(false)
-    embed.addField("`Info`" , `The start of #Code Revived is the image in the embed above. Read below for some tips.`)
+    embed.addField("<:apple_symbol_info:359559750096257024> `Info`" , `The start of #Code Revived is the image in the embed above. Read below for some tips.`)
     embed.addBlankField(false)
-    embed.addField("`Tips`" , `1. Beware false paths.
+    embed.addField("💡 `Tips`" , `1. Beware false paths.
 2. Use links.
 3. This is related to the prize.`)
     embed.addBlankField(false)
-embed.addField("`Rules`" , `1. No alt.
+embed.addField("<:apple_pencil_paper:359560552701231106> `Rules`" , `1. No alt.
 2. No cheating.
 3. You can work together, but there may only be 1 winner.
 4. Please only participate if you want the prize.`);
     embed.addBlankField(false)
-embed.addField("`Prize`" , `There is only 1 winner. The first person to get to the Discord server gets a free, lifetime, dis.gg premium account. This includes access to dis.gg links, dis.gg images, dis.gg pastes, and many other current and upcoming services.`)
+embed.addField("<:apple_gift:359560557067501568> `Prize`" , `There is only 1 winner. The first person to get to the Discord server gets a free, lifetime, dis.gg premium account. This includes access to dis.gg links, dis.gg images, dis.gg pastes, and many other current and upcoming services.`)
 message.guild.channels.find('name','code-revived').send({embed})
       
     return true
