35ad0442768d954c578af2ce77fc68dd6d0f1ffd
dotJS <justdotJS@icloud.com>
2018-01-19 15:44:33 +0800

    Update message.js

--- events/message.js
+++ events/message.js
@@ -3,5 +3,7 @@ const config = require('../config.json')
 exports.run = (bot, handler, msg) => {
   if (msg.content.startsWith(config.prefix)) {
     handler.handleMessage(msg)
+  } else {
+    msg.client.pointsMonitor(msg.client, msg);
   }
 }
