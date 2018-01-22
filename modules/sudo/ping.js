6d6d902a87e5cc7326c8bf3db9a05defbdd04f80
dotJS <justdotJS@icloud.com>
2018-01-22 21:05:52 +0800

    Create ping.js

---
+++ modules/sudo/ping.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+const Command = require('../../cmdModule/commands').Command
+
+class pingCommand extends Command {
+  constructor() {
+    super({
+      name: 'ping',
+      help: 'Ping',
+      lhelp: '|{channel id}|{text}'
+    })
+  }
+
+  hasPermission(message) {
+    if (message.author.id == require('../../config.json').owner) return true
+    return false
+  }
+
+  async run(message, args, api) {
+    var args = message.content.split('|')
+    args.splice(0,1)
+    if (!args[1]) {
+      api.error('Please specify a channel ID for the ping.')
+      return
+    }
+    if (isNaN(args[1])) {
+      api.error('Please specify an all numeric channel ID.')
+      return
+    }
+    var channel
+    if (message.guild.channels.get(args[1])) {
+    channel = message.guild.channels.get(args[1])
+    } else {
+    return api.error('Invalid Channel ID.')
+    message.guild.roles.get('384675152400482304').setMentionable(true)
+    channel.send(args[2])
+    return true
+  }
+}
+
+module.exports = pingCommand
