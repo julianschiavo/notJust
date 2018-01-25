49d72c5f634cdfc8a0e9963fe59fd295bba67257
dotJS <justdotJS@icloud.com>
2018-01-25 11:20:06 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -118,8 +118,24 @@ bot.dispatcher = ''
 //});
 //});
 
-bot.on("messageReactionAdd", (reaction) => {
-  console.log(reaction.message.id);
+bot.on("messageReactionAdd", (reaction, user) => {
+  var role
+  var given
+  if (reaction.message.id == '405912702855282698') {
+    if (reaction.emoji.id == '405909843098992650') {
+      role = reaction.message.guild.roles.get('384675152400482304')
+    } else if (reaction.emoji.id == '405911142012026891') {
+      role = reaction.message.guild.roles.get('404981161241214977')
+    }
+    if (user.hasRole(role)) {
+      user.removeRole(role)
+      given = ' You have left @'
+    } else {
+      user.addRole(role)
+      given = ' You have joined @'
+    }
+    user.send(reaction.emoji.toString() + given + role.name + '.')
+  }
 });
 
 
