5358c904479051a5fafb4ad45d33f619c7c411f9
dotJS <justdotJS@icloud.com>
2018-01-25 10:32:44 +0800

    Create messageReactionAdd.js

---
+++ events/messageReactionAdd.js
@@ -0,0 +1,23 @@
+const config = require('../config.json')
+const ta = require('../timeago.js')
+const Discord = require('discord.js')
+
+exports.run = (reaction, user) => {
+  var role
+  var given
+  if (reaction.message.id == '405912702855282698' && reaction.message.guild.id == '268970339948691456') {
+    if (reaction.emoji.id == '405909843098992650') {
+      role = message.guild.roles.get('384675152400482304')
+    } else if (reaction.emoji.id == '405911142012026891') {
+      role = message.guild.roles.get('404981161241214977')
+    }
+    if (user.hasRole(role)) {
+      user.removeRole(role)
+      given = ' You have left @'
+    } else {
+      user.addRole(role)
+      given = ' You have joined @'
+    }
+    user.send(reaction.emoji.toString() + given + role.name + '.')
+  }
+}
