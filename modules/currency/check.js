d845003f69e8b49e781bddc3e7782d77ee7b17fe
dotJS <justdotJS@icloud.com>
2018-02-17 22:35:45 +0800

    Update check.js

--- modules/currency/check.js
+++ modules/currency/check.js
@@ -59,9 +59,25 @@ class checkCommand extends Command {
     message.guild.fetchInvites().then(inv => {
       var uses = inv.filter(g => g.code == invi).map(g => g.uses)
       curr = message.client.currency.get(user.id)
-      curr.amount = (curr.amount + ((uses - past) * 100))
+      curr.amount = (curr.amount + ((uses - past) * 200)) /* change back to 100 after invite-a-thon */
       curr.pastUses = uses
       message.client.currency.set(user.id, curr)
+      
+      /* temporary invite-a-thon section */
+      var dots = message.client.dots.get(user.id) || { dots: 0, level: 0, time: 0 };
+      dots.dots = (dots.dots + ((uses - past) * 10))
+      var desiredLevel = dots.level + 1
+      var xpLeft = dots.dots - (5 / 6 * desiredLevel * (2 * desiredLevel * desiredLevel + 27 * desiredLevel + 91))
+      if (xpLeft >= 0) {
+        message.channel.send(`**${message.author.tag}** is now **Level ${desiredLevel}**!`).then(msg => {msg.delete(10000)}).catch(err => console.error(err))
+        dots.level = desiredLevel;
+      }
+      if (dots.level >= '10') {
+        user.addRole(message.guild.roles.find('name','color'))
+      }
+      message.client.dots.set(user.id,dots)
+      /* end temp section */
+      
       curr = message.client.currency.get(user.id)
       let embed = new Discord.RichEmbed()
       embed.setTitle('<:goldbar:383480100282171392> `Gold Bar Status for ' + user.user.tag + '`')
